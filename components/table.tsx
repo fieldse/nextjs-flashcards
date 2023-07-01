2d2e25fa16acebf88984020d8dfab2cb9539dc7c
Matt <mattfields+dev@pm.me>
2023-07-01 15:41:32 +1000

    don't seed directly in the view

--- components/table.tsx
+++ components/table.tsx
@@ -2,7 +2,6 @@ import { sql } from '@vercel/postgres';
 import { timeAgo } from '@/lib/utils';
 import Image from 'next/image';
 import RefreshButton from './refresh-button';
-import { seed } from '@/lib/seed';
 
 export default async function Table() {
   let data;
@@ -11,15 +10,7 @@ export default async function Table() {
   try {
     data = await sql`SELECT * FROM users`;
   } catch (e: any) {
-    if (e.message === `relation "users" does not exist`) {
-      console.log('Table does not exist, creating and seeding it with dummy data now...');
-      // Table is not created yet
-      await seed();
-      startTime = Date.now();
-      data = await sql`SELECT * FROM users`;
-    } else {
-      throw e;
-    }
+    throw new Error(`get users failed:` + e.message);
   }
 
   const { rows: users } = data;
