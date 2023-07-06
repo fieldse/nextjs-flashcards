3dbc25e099ad52ea36932b8b7a35967146172917
Matt <mattfields+dev@pm.me>
2023-07-06 23:46:19 +1000

    improve styles

--- components/single-card/single-card.tsx
+++ components/single-card/single-card.tsx
@@ -4,6 +4,7 @@ import { Heading2 } from '@/components/headers';
 import { Card } from '@/server/types';
 import { useEffect, useState } from 'react';
 import { HiOutlineChevronDoubleRight } from 'react-icons/hi';
+import { HiOutlineSpeakerWave } from 'react-icons/hi2';
 
 export default function SingleCard({ card }: { card: Card }) {
   const [hidden, setHidden] = useState(true);
@@ -15,14 +16,10 @@ export default function SingleCard({ card }: { card: Card }) {
     <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
       <div className="flex flex-col justify-between">
         <span className="flex flex-row justify-between">
-          <Heading2 className="md:text-6xl grow-0"> {card.headword || 'no headword'}</Heading2>
-
-          {/* Definition */}
-          <span className={`${hidden ? 'hidden' : ''} flex justify-end`}>
-            <span className="">
-              <p className="text-2xl">{card.definition || 'no definition'}</p>
-              <p className="text-xl">(n.)</p>
-            </span>
+          {/* Title headword */}
+          <span className="flex items-center">
+            <Heading2 className=" md:text-6xl grow-0">{card.headword || 'no headword'}</Heading2>
+            {!hidden && <HiOutlineSpeakerWave className="ml-4 w-6 h-6" />}
           </span>
 
           {/* Next button */}
@@ -31,24 +28,29 @@ export default function SingleCard({ card }: { card: Card }) {
               setHidden(!hidden);
             }}
           >
-            {/* SHow button */}
-            {hidden ? (
-              <HiOutlineChevronDoubleRight className="w-12 h-12 " />
-            ) : (
-              <span className="w-12 h-12 "></span>
-            )}
+            {/* Icon */}
+            {hidden && <HiOutlineChevronDoubleRight className="w-12 h-12 " />}
           </button>
+
+          {/* Definition */}
+          <span className={`${hidden ? 'hidden' : ''} flex justify-end`}>
+            <span className="">
+              <span className="text-2xl">{card.definition || 'no definition'}</span>
+              <span className="ml-2 text-xl">(n.)</span>
+            </span>
+          </span>
         </span>
 
+        {/* Scoring button */}
         {!hidden && (
           <div className="flex relative top-6 justify-around w-full my-8">
-            <button className="rounded-md shadow-md p-2 px-4 text-2xl bg-green-500 text-white">
+            <button className="rounded-md shadow-lg shadow-gray-400 drop-shadow-lg p-2 px-4 text-2xl bg-green-500 text-white">
               easy
             </button>
-            <button className="rounded-md shadow-md p-2 px-4 text-2xl bg-blue-500 text-white">
+            <button className="rounded-md shadow-lg   shadow-gray-400 drop-shadow-lg  p-2 px-4 text-2xl bg-blue-500 text-white">
               okay
             </button>
-            <button className="rounded-md shadow-md p-2 px-4 text-2xl bg-red-600 text-white">
+            <button className="rounded-md shadow-lg shadow-gray-400 drop-shadow-lg  p-2 px-4 text-2xl bg-red-600 text-white">
               hard
             </button>
           </div>
