14f0cc75f257b8f3dd8ae035f25741a9ae7f929a
Matt <mattfields+dev@pm.me>
2023-07-06 11:58:34 +1000

    styling

--- app/decks/decks-table.tsx
+++ app/decks/decks-table.tsx
@@ -30,9 +30,9 @@ export async function DecksTable() {
       <div className="divide-y divide-gray-900/5">
         {decks.map((deck) => (
           <div key={deck.title} className="flex items-start justify-between py-3">
-            <div className="flex items-center space-x-4 w-full">
+            <div className="flex items-center justify-between space-x-4 w-full">
+              {/* Deck title and icon */}
               <span className="flex flex-col">
-                {/* Deck title and icon */}
                 <span>
                   <HiOutlineBookOpen
                     className="inline-block mr-1 w-12 h-12 text-blue-500"
@@ -40,13 +40,13 @@ export async function DecksTable() {
                   />
                   <span className="text-xl font-semibold">{deck.title}</span>
                 </span>
-
-                {/* Card count, created date */}
-                <span className="w-full grow flex mt-6 leading-none space-between items-baseline">
-                  <span className="text-sm text-gray-400 grow">total cards: {deck.cardCount}</span>
-                  <span className="text-sm text-gray-500">{timeAgo(deck.createdAt)}</span>
-                </span>
+                <span className="text-sm text-gray-400 grow">total cards: {deck.cardCount}</span>
               </span>
+
+              {/* Card count, created date */}
+              <div className="flex mt-6 leading-none space-between items-baseline">
+                <span className="text-sm text-gray-500">{timeAgo(deck.createdAt)}</span>
+              </div>
             </div>
           </div>
         ))}
