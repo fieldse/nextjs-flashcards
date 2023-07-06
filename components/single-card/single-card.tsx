da69d64a4065558bac9fdcdf2b27c0117a8f410e
Matt <mattfields+dev@pm.me>
2023-07-06 23:54:15 +1000

    add simple linking with no preloading

--- components/single-card/single-card.tsx
+++ components/single-card/single-card.tsx
@@ -12,6 +12,8 @@ export default function SingleCard({ card }: { card: Card }) {
     console.log('inside SingleCard: ', JSON.stringify(card, null, 2));
   });
   console.log(`card: `, card);
+  const partOfSpeech = '(n.)'; // FIXME: placeholder
+  const nextCardId = card.id + 1;
   return (
     <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
       <div className="flex flex-col justify-between">
@@ -33,10 +35,10 @@ export default function SingleCard({ card }: { card: Card }) {
           </button>
 
           {/* Definition */}
-          <span className={`${hidden ? 'hidden' : ''} flex justify-end`}>
+          <span className={`${hidden ? 'hidden' : ''} flex flex-col justify-center`}>
             <span className="">
               <span className="text-2xl">{card.definition || 'no definition'}</span>
-              <span className="ml-2 text-xl">(n.)</span>
+              <span className="ml-2 text-xl">{partOfSpeech}</span>
             </span>
           </span>
         </span>
@@ -44,15 +46,21 @@ export default function SingleCard({ card }: { card: Card }) {
         {/* Scoring button */}
         {!hidden && (
           <div className="flex relative top-6 justify-around w-full my-8">
-            <button className="rounded-md shadow-lg shadow-gray-400 drop-shadow-lg p-2 px-4 text-2xl bg-green-500 text-white">
-              easy
-            </button>
-            <button className="rounded-md shadow-lg   shadow-gray-400 drop-shadow-lg  p-2 px-4 text-2xl bg-blue-500 text-white">
-              okay
-            </button>
-            <button className="rounded-md shadow-lg shadow-gray-400 drop-shadow-lg  p-2 px-4 text-2xl bg-red-600 text-white">
-              hard
-            </button>
+            <a href={`/card/${nextCardId}`}>
+              <button className="rounded-md shadow-lg shadow-gray-400 drop-shadow-lg p-2 px-4 text-2xl bg-green-500 text-white">
+                easy
+              </button>
+            </a>
+            <a href={`/card/${nextCardId}`}>
+              <button className="rounded-md shadow-lg   shadow-gray-400 drop-shadow-lg  p-2 px-4 text-2xl bg-blue-500 text-white">
+                okay
+              </button>
+            </a>
+            <a href={`/card/${nextCardId}`}>
+              <button className="rounded-md shadow-lg shadow-gray-400 drop-shadow-lg  p-2 px-4 text-2xl bg-red-600 text-white">
+                hard
+              </button>
+            </a>
           </div>
         )}
       </div>
