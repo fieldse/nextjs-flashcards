c7a188c96e81b9bc22d0f2b7a3e6348ceb2bf727
Matt <mattfields+dev@pm.me>
2023-07-06 11:13:26 +1000

    add rpc for decks with card counts

--- rpc/decks.ts
+++ rpc/decks.ts
@@ -1,23 +1,43 @@
 // Queries for the decks table
+import { Card, Deck, DeckWithCardCount } from '@/server/types';
 import { sql } from '@vercel/postgres';
 
 /**
  * Get all decks
  */
 export async function getAll() {
-  return await sql`SELECT * FROM decks`;
+  return await sql<Deck>`SELECT * FROM decks`;
+}
+
+/**
+ * Get all decks with card counts
+ */
+export async function getAllWithCardCounts() {
+  return await sql<DeckWithCardCount>`
+  SELECT 
+    d.*,
+    c.cardCount
+  FROM decks d
+  LEFT JOIN (
+    SELECT 
+      deck.id "deck_id",
+      COUNT(*) "cardCount"
+    FROM cards cc
+    WHERE cc.deck_id = d.id
+  ) c ON c.deck_id = d.id
+  `;
 }
 
 /**
  * Get a single deck by id
  */
 export async function get(id: string) {
-  return await sql`SELECT * FROM decks WHERE id = ${id}`;
+  return await sql<Deck>`SELECT * FROM decks WHERE id = ${id}`;
 }
 
 /**
  * Get all cards for given deck
  */
 export async function getDeckCards(id: string) {
-  return await sql`SELECT * FROM cards WHERE deck_id = ${id}`;
+  return await sql<Card>`SELECT * FROM cards WHERE deck_id = ${id}`;
 }
