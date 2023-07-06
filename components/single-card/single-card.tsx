ce6438605cff7512ef3ae0952b957ba2329c124c
Matt <mattfields+dev@pm.me>
2023-07-06 18:51:49 +1000

    working data fetching in single card view

--- components/single-card/single-card.tsx
+++ components/single-card/single-card.tsx
@@ -2,13 +2,18 @@
 
 import { Heading2 } from '@/components/headers';
 import { Card } from '@/server/types';
+import { useEffect } from 'react';
 
 export default function SingleCard({ card }: { card: Card }) {
+  useEffect(() => {
+    console.log('inside SingleCard: ', JSON.stringify(card, null, 2));
+  });
+  console.log(`card: `, card);
   return (
     <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
       <div className="flex flex-col justify-between">
-        <Heading2> {card.headword}</Heading2>
-        <p className="text-2xl">{card.definition}</p>
+        <Heading2> {card.headword || 'no headword'}</Heading2>
+        <p className="text-2xl">{card.definition || 'no definition'}</p>
       </div>
     </div>
   );
--- pages/card/[id]/index.tsx
+++ pages/card/[id]/index.tsx
@@ -2,6 +2,7 @@ import SingleCard from '@/components/single-card/single-card';
 import { Card } from '@/server/types';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import * as rpc from '@/rpc';
+import { Heading2 } from '@/components/headers';
 
 // Returns a single card view
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -14,6 +15,10 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 async function getData(id: string): Promise<{ card: Card }> {
   const data = await rpc.cards.get(id);
+  console.log(`=== debug: data: `, JSON.stringify(data, null, 2));
+  if (!data) {
+    throw new Error(`failed to get card data: card_id: ${id}`);
+  }
   return {
     card: JSON.parse(JSON.stringify(data)),
   };
@@ -32,12 +37,18 @@ export const getStaticProps: GetStaticProps = async (ctx: any) => {
       props: { hasError: true },
     };
   }
-  return { props: { card: data } };
+  const { card } = data;
+  return { props: { card } };
 };
 
 /**
  * View for a single card page
  */
 export default function SingleCardPage({ card }: { card: Card }) {
-  return <SingleCard card={card} />;
+  return (
+    <div>
+      <Heading2>Should be a card here</Heading2>
+      <SingleCard card={card} />
+    </div>
+  );
 }
