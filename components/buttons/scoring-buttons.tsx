ac1ffa532bef568031cb3139c47c2050c0f337f7
Matt <mattfields+dev@pm.me>
2023-07-07 16:15:25 +1000

    add icons to scoring buttons

--- components/buttons/scoring-buttons.tsx
+++ components/buttons/scoring-buttons.tsx
@@ -1,6 +1,8 @@
 // Buttons for self-scoring on a review session
 
+import { HiFaceFrown, HiOutlineFaceFrown, HiOutlineFaceSmile } from 'react-icons/hi2';
 import { ButtonSolid } from './button';
+import { HiOutlineThumbUp } from 'react-icons/hi';
 
 /**
  * Set of three buttons for self-scoring on a review session
@@ -10,13 +12,28 @@ export function ScoringButtons({ nextCardId }: { nextCardId: number }) {
   return (
     <div className="flex relative top-6 justify-around w-full my-8">
       <a href={`/card/${nextCardId}`}>
-        <ButtonSolid color="green-dark">Easy</ButtonSolid>
+        <ButtonSolid className="px-6" color="green-dark">
+          <span className="flex items-center">
+            Easy
+            <HiOutlineFaceSmile className="ml-3" />
+          </span>
+        </ButtonSolid>
       </a>
       <a href={`/card/${nextCardId}`}>
-        <ButtonSolid color="blue-dark">Okay</ButtonSolid>
+        <ButtonSolid className="px-6" color="blue-dark">
+          <span className="flex items-center">
+            Okay
+            <HiOutlineThumbUp className="ml-3" />
+          </span>
+        </ButtonSolid>
       </a>
       <a href={`/card/${nextCardId}`}>
-        <ButtonSolid color="red">Hard</ButtonSolid>
+        <ButtonSolid className="px-6" color="red">
+          <span className="flex items-center">
+            Hard
+            <HiOutlineFaceFrown className="ml-3" />
+          </span>
+        </ButtonSolid>
       </a>
     </div>
   );
