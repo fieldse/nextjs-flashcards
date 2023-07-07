ff2e1daf022803aa5bb7b36293853a17c25c4072
Matt <mattfields+dev@pm.me>
2023-07-07 15:14:17 +1000

    add prev/next card navigation

--- components/single-card/single-card.tsx
+++ components/single-card/single-card.tsx
@@ -3,45 +3,77 @@
 import { Heading2 } from '@/components/headers';
 import { Card } from '@/server/types';
 import { useEffect, useState } from 'react';
-import { HiOutlineChevronDoubleRight } from 'react-icons/hi';
 import { HiOutlineSpeakerWave } from 'react-icons/hi2';
 import { ScoringButtons } from '../buttons';
-import { NextButtonArrow, NextButtonDoubleArrow } from '../buttons/navigation-buttons';
+import {
+  NextButtonArrow,
+  NextButtonDoubleArrow,
+  PrevButtonDoubleArrow,
+} from '../buttons/navigation-buttons';
+import { useRouter } from 'next/router';
 
-export default function SingleCard({ card }: { card: Card }) {
-  const [hidden, setHidden] = useState(true);
-  useEffect(() => {
-    console.log('inside SingleCard: ', JSON.stringify(card, null, 2));
-  });
-  console.log(`card: `, card);
-  const partOfSpeech = '(n.)'; // FIXME: placeholder
-  const nextCardId = card.id + 1;
+type Props = {
+  card: Card;
+  // fixm: urls should have some pre-rendering or checking
+  nextCardId?: number | null;
+  prevCardId?: number | null;
+  showScoreButtons?: boolean;
+};
+
+/**
+ * Individual card view, with possibility for browsing and self-scoring
+ */
+export default function SingleCard({
+  card,
+  nextCardId,
+  prevCardId,
+  showScoreButtons = true,
+}: Props) {
+  const [showDefinition, setShowDefinition] = useState(false);
+  const router = useRouter();
+
+  // FIXME: placeholder
+  const partOfSpeech = '(n.)';
+
+  // Go to previous or next card
+  const navigateCardAction = (cardId: number) => {
+    return () => router.replace(`/card/${cardId}`);
+  };
+
   return (
     <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
       <div className="flex flex-col justify-between">
         <span className="flex flex-row justify-between">
+          {/* Previous card nav button */}
+          {!!prevCardId && <PrevButtonDoubleArrow action={navigateCardAction(prevCardId)} />}
+
           {/* Title headword */}
           <span className="flex items-center">
             <Heading2 className=" md:text-6xl grow-0">{card.headword || 'no headword'}</Heading2>
 
             {/* Pronunciation --placeholder  */}
-            {!hidden && <HiOutlineSpeakerWave className="ml-4 w-6 h-6" />}
+            {!showDefinition && <HiOutlineSpeakerWave className="ml-4 w-6 h-6" />}
           </span>
 
-          {/* Next button */}
-          {hidden && <NextButtonArrow action={() => setHidden(!hidden)} />}
+          {/* "Show definition" button */}
+          {showDefinition && <NextButtonArrow action={() => setShowDefinition(true)} />}
 
           {/* Definition */}
-          <span className={`${hidden ? 'hidden' : ''} flex flex-col justify-center`}>
+          <span className={`${showDefinition ? 'hidden' : ''} flex flex-col justify-center`}>
             <span className="">
               <span className="text-2xl">{card.definition || 'no definition'}</span>
               <span className="ml-2 text-xl">{partOfSpeech}</span>
             </span>
           </span>
+
+          {/* Next card nav button */}
+          {!!nextCardId && <NextButtonDoubleArrow action={navigateCardAction(nextCardId)} />}
         </span>
 
-        {/* Scoring button */}
-        {!hidden && <ScoringButtons nextCardId={nextCardId} />}
+        {/* Self-scoring buttons */}
+        {showDefinition && showScoreButtons && nextCardId && (
+          <ScoringButtons nextCardId={nextCardId} />
+        )}
       </div>
     </div>
   );
--- pages/card/[id]/index.tsx
+++ pages/card/[id]/index.tsx
@@ -3,23 +3,43 @@ import { Card } from '@/server/types';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import * as rpc from '@/rpc';
 
+type SingleCardProps = {
+  card: Card;
+  nextCardId: number | null;
+  prevCardId: number | null;
+};
+
+const getAllCards = async () => {
+  return await rpc.cards.getAll({ limit: 1000 }); // fixme: card limits
+};
+
 // Returns a single card view
 export const getStaticPaths: GetStaticPaths = async () => {
-  const allCards = await rpc.cards.getAll({ limit: 50 });
+  const allCards = await getAllCards();
   return {
     paths: allCards.rows.map((card) => `/card/${card.id}`),
     fallback: true,
   };
 };
 
-async function getData(id: string): Promise<{ card: Card }> {
-  const data = await rpc.cards.get(id);
-  console.log(`=== debug: data: `, JSON.stringify(data, null, 2));
-  if (!data) {
+async function getData(id: string): Promise<SingleCardProps> {
+  const card = await rpc.cards.get(id); // get the individual card
+
+  // get all cards to check if we have a next card ID
+  const allCards = await getAllCards();
+
+  // Prev/next cards, if existing
+  const nextCardId = allCards.rows.find((x) => x.id === card.id + 1)?.id || null; // null for allowing JSON serialization
+  const prevCardId = allCards.rows.find((x) => x.id === card.id - 1)?.id || null;
+
+  console.log(`=== debug: data: `, JSON.stringify(card, null, 2));
+  if (!card) {
     throw new Error(`failed to get card data: card_id: ${id}`);
   }
   return {
-    card: JSON.parse(JSON.stringify(data)),
+    card: JSON.parse(JSON.stringify(card)),
+    nextCardId,
+    prevCardId,
   };
 }
 
@@ -31,18 +51,17 @@ export const getStaticProps: GetStaticProps = async (ctx: any) => {
     };
   }
   const data = await getData(id);
-  if (!data) {
+  if (!data.card) {
     return {
       props: { hasError: true },
     };
   }
-  const { card } = data;
-  return { props: { card } };
+  return { props: { ...data } };
 };
 
 /**
  * View for a single card page
  */
-export default function SingleCardPage({ card }: { card: Card }) {
-  return <SingleCard card={card} />;
+export default function SingleCardPage({ card, nextCardId, prevCardId }: SingleCardProps) {
+  return <SingleCard card={card} nextCardId={nextCardId} prevCardId={prevCardId} />;
 }
