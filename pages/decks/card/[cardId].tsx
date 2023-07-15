2eb04ae0221f74296c3d6334d73cbbcd08a5820f
Matt <mattfields+dev@pm.me>
2023-07-16 00:21:27 +1000

    remove debugs

--- pages/decks/card/[cardId].tsx
+++ pages/decks/card/[cardId].tsx
@@ -1,17 +1,13 @@
 import * as rpc from '@/rpc';
 import { Card } from '@/server/types';
-import { GetStaticPaths, GetStaticProps } from 'next';
-import { logDebug } from '@/lib/utils';
+import { GetStaticProps } from 'next';
 import SingleCard from '@/components/single-card/single-card';
-import URLS from '@/lib/urls';
 
 export const getStaticProps: GetStaticProps = async (ctx: any) => {
   const { cardId } = ctx.params;
-  logDebug(`=== (card) getStaticProps: cardId`, cardId);
   try {
     const data = await await rpc.cards.get(cardId);
     const parsed = JSON.parse(JSON.stringify(data));
-    logDebug(`=== (card) getStaticProps: data`, JSON.stringify(parsed, null, 2));
     return { props: { card: parsed } };
   } catch (err) {
     console.log(`getStaticProps: error: ${err}`);
